bdd06fb9d327752b6056f73c388544400ff781d9
misiuklimek <113717955+misiuklimek@users.noreply.github.com>
2023-01-19 21:31:35 -0600

    AHHH

--- UN7/TicketMasterLab/TicketMaster.java
+++ UN7/TicketMasterLab/TicketMaster.java
@@ -3,6 +3,7 @@ package UN7.TicketMasterLab;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicketMaster {
@@ -11,42 +12,65 @@ public class TicketMaster {
     private String fileName;
     boolean ifQuit;
 
-    public static void continueSite(Boolean ifQuit, ArrayList<Show> shows){
+    public static String recordResponseString(){
         Scanner input = new Scanner(System.in);
-        int answer = input.nextInt();
+        String answer = input.nextLine();
+        return answer;
+    }
+    public static int recordResponse(){
+        Scanner input = new Scanner(System.in);
+        boolean tryAgain = true;
+        int answer = 0;
+        while (tryAgain){
+            try {
+                answer = input.nextInt();
+                if (answer < 7 && answer > 0){
+                    tryAgain = false;
+                } else{
+                    System.out.println("Invalid Entry, please try a number between 1-6!");
+                }
+            } catch (Exception e){
+                System.out.println("Invalid Entry, please try again using an integer!");
+                input.nextLine();
+            }
+        }
+        return answer;
+    }
+    public static void continueSite(Boolean ifQuit, ArrayList<Show> shows){
+        int answer = recordResponse();
         if (answer == 6){
             ifQuit = true;
         }
         while (!ifQuit){
 
-
             if(answer == 5){
-                System.out.println("Shows are sorted by Price (Descending)");
-                sortByPriceDescending();
-                System.out.println(shows.toString());
+                System.out.println("\nShows are sorted by Price (Descending)");
+                sortByPriceDescending(shows);
+                System.out.println(optionsToString(shows));
                 System.out.println("\n"+options());
             } else if (answer == 4) {
-                System.out.println("Shows are sorted by Price (Ascending)");
-                sortByPriceAscending();
-                System.out.println(shows.toString());
+                System.out.println("\nShows are sorted by Price (Ascending)");
+                sortByPriceAscending(shows);
+                System.out.println(optionsToString(shows));
                 System.out.println("\n"+options());
             } else if (answer == 3) {
-                System.out.println("Shows are sorted by Performer (Z-A)");
-                sortByPerformerZ();
-                System.out.println(shows.toString());
+                System.out.println("\nShows are sorted by Performer (Z-A)");
+                sortByPerformerZ(shows);
+                System.out.println(optionsToString(shows));
                 System.out.println("\n"+options());
             } else if (answer == 2) {
-                System.out.println("Shows are sorted by Performer (A-Z)");
-                sortByPerformerA();
-                System.out.println(shows.toString());
+                System.out.println("\nShows are sorted by Performer (A-Z)");
+                sortByPerformerA(shows);
+                System.out.println(optionsToString(shows));
                 System.out.println("\n"+options());
             } else if (answer == 1) {
-                sortByCity();
-                System.out.println("Shows are sorted by City");
-                System.out.println(shows.toString());
+                ArrayList<Show> temp = new ArrayList<>();
+                temp = sortByCity(shows);
+                System.out.println("\nShows are sorted by City");
+                System.out.println(optionsToString(temp));
                 System.out.println("\n"+options());
             }
-            answer = input.nextInt();
+            answer = recordResponse();
             if (answer == 6){
                 ifQuit = true;
             }
@@ -84,25 +108,38 @@ public class TicketMaster {
         }
     }
 
+//*
+    //public static void sortByPriceDescending(ArrayList<Show> shows){
+        //ArrayList<Integer> priceList = new ArrayList<>();
+        //for(Show prices : shows){
 
-    public static void sortByPriceDescending(){
+        //}
+      //  Arrays.sort(priceList);
+    //}
 
-    }
 
-    public static void sortByPriceAscending(){
-
-    }
-
-    public static void sortByPerformerA(){
-
-    }
-
-    public static void sortByPerformerZ(){
+    public static void sortByPriceAscending(ArrayList<Show> shows){
 
     }
 
-    public static void sortByCity(){
+    public static void sortByPerformerA(ArrayList<Show> shows){
 
+    }
+
+    public static void sortByPerformerZ(ArrayList<Show> shows){
+
+    }
+
+    public static ArrayList<Show> sortByCity(ArrayList<Show> shows){
+        String cityPicked = recordResponseString();
+        ArrayList<Show> outputArray = new ArrayList<>();
+        for (Show curShow : shows){
+            if (curShow.getCity().equalsIgnoreCase(cityPicked)){
+                outputArray.add(curShow);
+            }
+        }
+
+        return outputArray;
     }
 
     public void start(){
@@ -113,12 +150,12 @@ public class TicketMaster {
     }
     public static String options(){
         String output = "1. Search by city\n2. Sort by " +
-        "Performer (A-Z)\n3. Search by Performer (Z-A)\n4. Sort by Price (Low-High)\n5. Sort by Price (High-Low)\n6. Quit";
+        "Performer (A-Z)\n3. Search by Performer (Z-A)\n4. Sort by Price (Low-High)\n5. Sort by Price (High-Low)\n6. Quit\n";
         output += "\nEnter a value between 1-6:";
         return output;
     }
 
-    public String toString(){
+    public static String optionsToString(ArrayList<Show> shows){
         String output = "Date\t\tPrice\tQty\t Performer\t\t\t  City\n-------------------------------------------------------\n";
         for(Show concert : shows){
             output += concert.toString();
