6535c45e3ddeebeaf289e9426f720a27e1a9f7d2
juliapod23 <juliapod23@gmail.com>
2023-03-17 10:32:22 -0500

    lunch 3/17

--- U9/U9Lab/ProductDriver.java
+++ U9/U9Lab/ProductDriver.java
@@ -11,11 +11,15 @@ import java.util.Scanner;
 public class ProductDriver {
     public static void main(String[] args) {
         ArrayList<Product> products = new ArrayList<>();
-        Product product1 = new Product("Sephora","20",false,"Sky High");
+        Product product1 = new Product("Sephora",20,false,"Sky High Mascara");
         Product food1 = new Food("Wingstop",9,true,"Medium","BBQ Chicken");
-        Product shoe1 = new Shoes("Dick Ponds",130,"Mens","Running","Black",true,"Nike Pegasus 29");
+        Product shoe1 = new Shoes("Dick Ponds",130,false,"Mens","Running","Black",true,9.5,"Nike Pegasus 29");
         Product apparel1 = new Apparel("Claire's",15,true,"Womens","Formal","Gold","Hoop Earrings");
         Product clothing1 = new Clothing("Mens","Running","Red","T-Shirt");
+        Product product2 = new Product("Lush", 11,false,"Scrubee Body Scrub");
+        Product food2 = new Food("Starbucks",6,false,"Large","Pistachio Cold Brew");
+        Product shoe2 = new Shoes("Nike",110,true,"Womens","Sportstyle","White",true,7.5,"Air Force One");
+        Product apparel2 = new Apparel("Fossil",160,true,"Womens","Formal","Gold","Wristwatch");
 
 
         System.out.println("Welcome to the mall!\nWhat are you looking for today?");
